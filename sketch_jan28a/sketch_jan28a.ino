bfd87093fa67cb955b4578a452c9550eea16fa30
Crmayb2 <crmayb2@ilstu.edu>
2025-01-28 14:59:53 -0600

    Temp and Humidity with Buzzer

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -7,6 +7,7 @@
 U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/
 U8X8_PIN_NONE);
 DHT dht(3, DHT11);
+const int buzzerPin = 5;
 void setup() {
   dht.begin();
 u8x8.begin();
@@ -14,7 +15,30 @@ u8x8.setFlipMode(1);
 u8x8.clear();
 }
 void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  if (isnan(humidity) || isnan(temperature)) {
+    u8x8.clear();
+    u8x8.setCursor(0, 0);
+    u8x8.print("Failed to read sensor");
+  } else {
+    u8x8.clear();
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
+    u8x8.setCursor(0, 0);
+    u8x8.print("Temp: ");
+    u8x8.print(temperature, 1);
+    u8x8.print(" C");
+
+    u8x8.setCursor(0, 2);
+    u8x8.print("Humidity: ");
+    u8x8.print(humidity, 1);
+    u8x8.print(" %");
+  }
+  if (humidity > 70) {
+      tone(buzzerPin, 1000);
+    } else {
+      noTone(buzzerPin);
 u8x8.setFont(u8x8_font_chroma48medium8_r);
 u8x8.setCursor(0, 0);
-u8x8.print("Hello World!");
+}
 }
